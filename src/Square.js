d1cb4be53e75081c55da100de96db3192d1ded7c
vmantri83 <vmantri83@gmail.com>
2024-05-25 13:13:32 +0530

    added files

---
+++ src/Square.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// const Square = ({ value, onClick }) => (
+//   <button className="square" onClick={onClick}>
+//     {value}
+//   </button>
+// );
+
+// export default Square;
+
+import React from "react";
+// value="X";
+const Square = () => (
+  <button className="square">{/* just for testing */ }</button>
+);
+
+export default Square;
